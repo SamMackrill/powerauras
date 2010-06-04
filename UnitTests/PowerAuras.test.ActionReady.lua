dcdba249d583ebfc9dc7152e8cedc400b4401783
Smacker <Smacker@localhost>
2010-06-04 12:18:02 +0100

    new duration calc

---
+++ UnitTests/PowerAuras.test.ActionReady.lua
@@ -0,0 +1,10 @@
+﻿	
+	function TestPA:test_ActionReady_ShouldShow()
+		self:SetUp();
+		TestPA.Debug = true;
+		self:SetupFor("ActionUsable");
+		
+		self:AuraTest(15,  true, "Action Ready");
+
+		self:TearDown();
+	end
---
+++ UnitTests/PowerAuras.test.Utils.lua
@@ -0,0 +1,28 @@
+﻿
+	function TestPA:test_CopyTable()
+		self:SetUp();
+		local PB = {};
+		local PAStore = {};
+		PB["Data"] = {a="a", b="b", c={d="d", e="e"}};
+		PAStore["Data"] = PowaAuras:CopyTable(PB["Data"]);
+		PB.Data.a = "Z";
+		assertEquals(PAStore.Data.a, "a", "a");
+		assertEquals(PAStore.Data.b, "b", "b");
+		assertEquals(PAStore.Data.c.e, "e", "c.e");
+		assertEquals(PAStore.Data.c.f, nil, "c.f");
+		self:TearDown()
+	end
+
+	function TestPA:test_OperatorCollection()
+		self:SetUp();
+		assertEquals(PowaAuras.allowedOperators["="], true, "=");
+		assertEquals(PowaAuras.allowedOperators["!"], true, "!");
+		assertEquals(PowaAuras.allowedOperators[">"], true, ">");
+		assertEquals(PowaAuras.allowedOperators["<"], true, "<");
+		assertEquals(PowaAuras.allowedOperators[">="], true, ">=");
+		assertEquals(PowaAuras.allowedOperators["<="], true, "<=");
+		assertEquals(PowaAuras.allowedOperators["-"], true, "-");
+		assertEquals(PowaAuras.allowedOperators["a"], nil, "a");
+		assertEquals(PowaAuras.allowedOperators[""], nil, "blank");
+		self:TearDown()
+	end
