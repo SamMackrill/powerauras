1c1d9510796bbb6e286315d050f2ce474bdd2bac
Smacker <Smacker@localhost>
2010-09-17 00:15:39 +0100

    S ==> T

--- BuildForRelease.bat
+++ BuildForRelease.bat
@@ -1,4 +1,4 @@
 cd ..
-"C:\Program Files\7-Zip\7z.exe" a -tzip -xr!PowerAuras\UnitTests\* -xr!PowerAuras\UnitTests -xr!?hg\* -x!PowerAuras\.hg* -x!PowerAuras\*.bat PowerAuras.zip PowerAuras\
+"C:\Program Files\7-Zip\7z.exe" a -tzip -xr!PowerAuras\UnitTests\* -xr!PowerAuras\UnitTests -xr!?hg\* -x!PowerAuras\.hg* -x!PowerAuras\*.bat -x!PowerAuras\*.session PowerAuras.zip PowerAuras\
 @pause
 @exit
