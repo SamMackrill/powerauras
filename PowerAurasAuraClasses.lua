808f110abc8b943f7e64b483326e8ee2a42dc2bf
Smacker <Smacker@localhost>
2010-10-12 20:46:57 +0100

    comments improved

--- PowerAurasAuraClasses.lua
+++ PowerAurasAuraClasses.lua
@@ -61,6 +61,8 @@ end);
 
 -- This is the set of values that will be exported with their default values
 -- Be very careful if you change this as it may break many old exports
+-- Stings must always be set as at least an empty string
+-- Numbers an booleans can be set interchangable (e.g. for tri-states)
 cPowaAura.ExportSettings = {
 	off = false,
 	
