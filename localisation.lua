0844adc9544aae804c5d891f95dd9e70e227f8b0
dyates92 <dyates92@localhost>
2011-02-18 19:10:17 +0000

    Sending/receiving auras fully implemented and working. Code is very messy, will improve later.

--- localisation.lua
+++ localisation.lua
@@ -727,16 +727,28 @@ PowaAuras:MergeTables(PowaAuras.Text,
 	},
 	
 	-- Export dialog
-	ExportDialogTopTitle = "Export Auras",
-	ExportDialogCopyTitle = "Press Ctrl-C to copy the below aura string.",
-	ExportDialogMidTitle = "Send to Player",
-	ExportDialogSendTitle = "Enter a player name below and click 'Send'.",
-	ExportDialogSendingTitle = "Sending...",
-	ExportDialogSendButton = "Send",
-	ExportDialogCancelButton = "Close",
+	ExportDialogTopTitle      = "Export Auras",
+	ExportDialogCopyTitle     = "Press Ctrl-C to copy the below aura string.",
+	ExportDialogMidTitle      = "Send to Player",
+	ExportDialogSendTitle1    = "Enter a player name below and click 'Send'.",
+	ExportDialogSendTitle2    = "Contacting %s...",      -- The 1/2/3/4 suffix denotes the internal status of the frame.
+	ExportDialogSendTitle3    = "%s has declined the offer.",
+	ExportDialogSendTitle4    = "Sending auras...",
+	ExportDialogSendTitle5    = "Send successful!",
+	ExportDialogSendButton1   = "Send",
+	ExportDialogSendButton2   = "Back",
+	ExportDialogCancelButton  = "Close",
 	-- Cross-client import dialog
-	PlayerImportDialogTopTitle = "You Have Auras!",
-	PlayerImportDialogDescTitle = "%s would like to send you some auras.",
+	PlayerImportDialogTopTitle       = "You Have Auras!",
+	PlayerImportDialogDescTitle1     = "%s would like to send you some auras.",
+	PlayerImportDialogDescTitle2     = "Receiving auras...",
+	PlayerImportDialogDescTitle3     = "The offer has expired.",
+	PlayerImportDialogDescTitle4     = "Select a page to save the auras to.",
+	PlayerImportDialogDescTitle5     = "Auras saved!",
+	PlayerImportDialogDescTitle6     = "No aura slots are available.",
+	PlayerImportDialogAcceptButton1  = "Accept",
+	PlayerImportDialogAcceptButton2  = "Save",
+	PlayerImportDialogCancelButton1  = "Reject",
 
 });
 
