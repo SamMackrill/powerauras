2c35ec6aec1255207a3dac640dd4084f326b436a
dyates92 <dyates92@localhost>
2011-02-28 21:29:17 +0000

    Fixed the added argument supplied by CLEU event in 4.1. Attempt to get Cast By Me added to Spell Alert, so that it works with Lava Surge and any other weird procs.

---
+++ UnitTests/PowerAuras.test.TimerClass.lua
@@ -0,0 +1,30 @@
+﻿
+	function TestPA:test_ExtractDigits()
+		self:SetUp();
+		
+		local deci, uni = cPowaTimer:ExtractDigits(0);
+		assertEquals(deci, 0, "0 deci");
+		assertEquals(uni, 0, "0 uni");
+		
+		local deci, uni = cPowaTimer:ExtractDigits(1);
+		assertEquals(deci, 0, "1 deci");
+		assertEquals(uni, 1, "1 uni");
+		
+		local deci, uni = cPowaTimer:ExtractDigits(20);
+		assertEquals(deci, 2, "20 deci");
+		assertEquals(uni, 0, "20 uni");
+		
+		local deci, uni = cPowaTimer:ExtractDigits(73);
+		assertEquals(deci, 7, "73 deci");
+		assertEquals(uni, 3, "73 uni");
+		
+		local deci, uni = cPowaTimer:ExtractDigits(0.5);
+		assertEquals(deci, 0, "0.5 deci");
+		assertEquals(uni, 0, "0.5 uni");
+		
+		local deci, uni = cPowaTimer:ExtractDigits(89.99);
+		assertEquals(deci, 8, "89.99 deci up");
+		assertEquals(uni, 9, "89.99 uni up");
+		
+		self:TearDown()
+	end	
