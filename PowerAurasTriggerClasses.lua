e81a7f575b9f11656d28f1552e2a862826d93608
dyates92 <dyates92@localhost>
2011-03-07 22:51:07 +0000

    Initial commit of decorator class, including functional AuraOpacity one.

--- PowerAurasTriggerClasses.lua
+++ PowerAurasTriggerClasses.lua
@@ -10,12 +10,7 @@ cPowaTrigger = PowaClass(function(trigger, auraId, triggerId)
 	trigger.AuraId           = auraId;
 	trigger.TriggerId        = triggerId;
 	trigger.Decorators       = {};
-	trigger.DecoratorsByType = {};
 	trigger.State            = false;
-	
-	for decorator, _ in pairs(PowaAuras.TriggerDecorators) do
-		trigger.DecoratorsByType[decorator] = {};
-	end	
 end);
 
 cPowaTrigger.ExportSettings = {
@@ -23,40 +18,37 @@ cPowaTrigger.ExportSettings = {
 	LowerCondition = ">=5",
 	UpperCondition = "<=6",
 }
--- Got bored, did some testing for implementing multiple condition checks.
-function cPowaTrigger:UpdateCondition()
-	-- -- -- Matches this: >=5 & < 500 & (>= 60 | <= 80 & (== 52 | != 51)
-	-- -- local regex = "(%s*([&|]?)%s*(%(*)%s*([<>=!]+)%s*([0-9]+)%s*(%)*))";
-	-- -- -- Remove pre-existing conditions.
-	-- -- wipe(self.Condition);
-	-- -- for full, join, paren_open, operator, value, paren_end in string.gmatch(self.ExportSettings.Condition, regex) do
-		-- -- -- Sanity check.
-		-- -- if(operator == "==" or operator == ">=" or operator == "<=" or operator == "<" or operator == ">" or operator == "!=") then
-			-- -- tinsert(self.Condition, { 
-				-- -- Operator = operator,
-				-- -- OpenDepth = strlen(paren_open or ""),
-				-- -- CloseDepth = strlen(paren_end or ""),
-				-- -- Value = value,
-				-- -- Join = join
-			-- -- });
-		-- -- end
-	-- -- end
-end
 
 function cPowaTrigger:Check(value)
-	
-	
-	
-	return false;
+	local result = (value >= 5 and value <= 6);
+	-- Attempt to activate our decorators.
+	for dType, decorator in pairs(self.Decorators) do
+		if(decorator) then
+			if(result == true and PowaAuras.Auras[self.AuraId]:ApplyDecorator(dType, self.TriggerId) == true) then
+				if(decorator.State == true) then
+					decorator:Update(value);
+				else
+					decorator:Activate(value);
+				end
+			elseif(result == false and decorator.State == true) then
+				decorator:Deactivate(value);
+				PowaAuras.Auras[self.AuraId]:RemoveDecorator(dtype, self.TriggerId);
+			end		
+		end
+	end
 end
 
 function cPowaTrigger:CreateDecorator(dType)
-
+	-- Make the decorator class.
+	local decorator = dType(self.AuraId, self.TriggerId);
+	UIErrorsFrame:AddMessage("Creating " .. decorator.Type .. "Decorator (" .. self.AuraId .. ", " .. self.TriggerId .. ")", 0.0, 1.0, 0.0);
+	self.Decorators[decorator.Type] = decorator;
+	return true;	
 end
 
-function cPowaTrigger:RemoveDecorator(id)
+-- -- function cPowaTrigger:RemoveDecorator(id)
 
-end
+-- -- end
 
 --[[
 =====cPowaTimerTrigger=====
@@ -65,7 +57,90 @@ Timer trigger type class.
 --]]
 cPowaTimerTrigger = PowaClass(cPowaTrigger, { Type = "Timer" });
 
--- -- function cPowaTimerTrigger:Check(value)
-	-- -- -- UIErrorsFrame:AddMessage("Checking " .. self.Type .. "Trigger (" .. self.AuraId .. "," .. self.TriggerId .. ") against value " .. value, 1.0, 0.0, 0.0);
-	-- -- return false;
+--[[
+=====cPowaDecorator========
+
+===========================
+--]]
+cPowaDecorator = PowaClass(function(decorator, auraId, triggerId)
+	if(not auraId or not triggerId or not PowaAuras.Auras[auraId] or not PowaAuras.Auras[auraId].Triggers[triggerId]) then return; end
+	-- PowaAuras:ShowText("Constructing Decorator type " .. (decorator.Type or 0));
+	-- Set up variables for trigger.
+	decorator.AuraId           = auraId;
+	decorator.TriggerId        = triggerId;
+	decorator.State            = false;
+	decorator.Value            = 0;
+end);
+
+cPowaDecorator.ExportSettings = {
+	Enabled = true,
+	IsGradual = true,
+}
+
+function cPowaDecorator:Activate(value)
+	-- Activate me.
+	self.State = true;
+	self.Value = value;
+	-- Update has logic for display stuff.
+	self:Update(value, true);
+end
+
+function cPowaDecorator:Deactivate(value)
+	self.State = false;
+	self.Value = value;
+	-- Final update.
+	self:Update(value);
+	UIErrorsFrame:AddMessage("Decorator (" .. self.AuraId .. ", " .. self.TriggerId .. ", " .. self.Type .. ") deactivating.", 1.0, 0.0, 0.0);
+end
+
+function cPowaDecorator:Update(value, firstRun)
+	-- Update value.
+	self.Value = value;
+end
+
+--[[
+=====cPowaAuraOpacityDecorator========
+Fix that later.
+===========================
+--]]
+cPowaAuraOpacityDecorator = PowaClass(cPowaDecorator, { Type = "AuraOpacity" });
+
+function cPowaAuraOpacityDecorator:Update(value, firstRun)
+	-- Update value.
+	self.Value = value;
+	-- Print msg.
+	if(firstRun) then
+		UIErrorsFrame:AddMessage("Decorator (" .. self.AuraId .. ", " .. self.TriggerId .. ", " .. self.Type .. ") activating.", 0.0, 1.0, 0.0);
+	else
+		UIErrorsFrame:AddMessage("Decorator (" .. self.AuraId .. ", " .. self.TriggerId .. ", " .. self.Type .. ") updating.", 0.0, 0.0, 1.0);
+	end
+	-- Change opacity.
+	local aura = PowaAuras.Auras[self.AuraId];
+	if(not aura) then return; end
+	local frame = aura:GetFrame();
+	if(not frame) then return; end
+	if(self.State == true) then
+		frame:SetAlpha(0.25);
+	else
+		frame:SetAlpha(math.min(aura.alpha, 0.99));
+	end
+end
+-- -- -- Testing script.
+-- -- for i=1,4 do
+   -- -- for j=1,1 do
+      -- -- PowaAuras.Auras[i]:CreateTrigger(cPowaTimerTrigger);
+      -- -- PowaAuras.Auras[i].Triggers[j]:CreateDecorator(cPowaAuraOpacityDecorator);
+   -- -- end
 -- -- end
+
+-- -- local f = LOL or CreateFrame("Frame", "LOL", UIParent);
+-- -- f.last = 0;
+-- -- f:SetScript("OnUpdate", function(self, elapsed)
+      -- -- f.last = f.last + elapsed;
+      -- -- if(f.last > 0.1) then
+         -- -- for i=1,4 do
+            -- -- PowaAuras.Auras[i]:SetTriggerCheck("Timer", random(1,6));
+         -- -- end
+         -- -- f.last = f.last - 0.1;
+      -- -- end
+-- -- end);
