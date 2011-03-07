c92bf083d6d2398691697bb812a66761e4b1d12b
dyates92 <dyates92@localhost>
2011-03-07 18:45:30 +0000

    Changes to how triggers are handled, added Create/Remove trigger functions.

--- PowerAurasTriggerClasses.lua
+++ PowerAurasTriggerClasses.lua
@@ -16,6 +16,7 @@ end);
 function cPowaTrigger:Check(value)
 	return false;
 end
+
 --[[
 =====cPowaTimerTrigger=====
 Timer trigger type class.
@@ -24,6 +25,30 @@ Timer trigger type class.
 cPowaTimerTrigger = PowaClass(cPowaTrigger, { Type = "Timer" });
 
 function cPowaTimerTrigger:Check(value)
-	-- PowaAuras:ShowText("Checking timer trigger (" .. self.AuraId .. "," .. self.TriggerId .. ") against value " .. value);
+	-- UIErrorsFrame:AddMessage("Checking " .. self.Type .. "Trigger (" .. self.AuraId .. "," .. self.TriggerId .. ") against value " .. value, 1.0, 0.0, 0.0);
+	value = math.random(2, 100);
+	if(value == 2) then return 2; end
+	if(value == 1) then return 1; end
 	return false;
 end
+
+-- Test script (run ingame)
+-- -- GLOBALCOUNTVAR = 25;
+
+-- -- for i=1,4 do
+   -- -- for j=1,10 do
+      -- -- PowaAuras.Auras[i]:CreateTrigger(cPowaTimerTrigger);
+   -- -- end
+-- -- end
+
+-- -- local f = LOL or CreateFrame("Frame", "LOL", UIParent);
+-- -- f.last = 0;
+-- -- f:SetScript("OnUpdate", function(self, elapsed)
+      -- -- f.last = f.last + elapsed;
+      -- -- if(f.last > 0.001) then
+         -- -- for i=1,4 do
+            -- -- PowaAuras.Auras[i]:SetTriggerCheck("Timer", random(1,100));
+         -- -- end
+         -- -- f.last = f.last - 0.001;
+      -- -- end
+-- -- end);
