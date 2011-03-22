6b76aa0049a71c6182658d285f5a4f43e31f3860
dyates92 <dyates92@localhost>
2011-03-22 23:04:42 +0000

    Fixed some frame positioning as a result of my epic fix. Renamed some locals in the updatelayout func.

--- PowerUI.lua
+++ PowerUI.lua
@@ -286,8 +286,8 @@ function PowaLayoutFrame_Init(frame)
 	end
 	-- Updates the layout of the frame.
 	frame.UpdateLayout = function(self)
-		local itemCount, column, columnOffset, offsetY, offsetX, modY = #(self.Items), 0, 1, 0, 0, 0;
-		for i=1,itemCount do
+		local iC, c, cO, oY, oX, mY = #(self.Items), 0, 1, 0, 0, 0;
+		for i=1,iC do
 			-- Some locals for width, height and the item...
 			local item, cW, cH = self.Items[i], 0, 0;
 			-- Store padding/margins in locals, as it's a long thing to write out and we need to calculate stuff from it a lot.
@@ -306,47 +306,46 @@ function PowaLayoutFrame_Init(frame)
 			mT = (mT > 0 and mT < 1 and self:GetWidth() * mT or mT);
 			mB = (mB > 0 and mB < 1 and self:GetWidth() * mB or mB);
 			-- Update column offset.
-			if(self.Columns < (column+columnOffset)) then
-				column = 1;
-				offsetX = 0;
-				offsetY = offsetY-modY;
-				modY = 0;
+			if(self.Columns < (c+cO)) then
+				c = 1;
+				oX = 0;
+				oY = oY-mY;
+				mY = 0;
 			else
-				column = column+columnOffset;
+				c = c+cO;
 			end
 			-- Calculate column height and width, obeying column span rules.
 			-- If the size is <= 1, then it's a fluid value based on container size. If no size is specified, default to item size.
-			if(column+(item.LayoutOpts["Columns"]-1) <= self.Columns) then
-				for j=column, column+(item.LayoutOpts["Columns"]-1) do
+			if(c+(item.LayoutOpts["Columns"]-1) <= self.Columns) then
+				for j=c, c+(item.LayoutOpts["Columns"]-1) do
 					cW = cW + (self.ColumnSizes[j]["X"] or 0);
 					cH = (cH > (self.ColumnSizes[j]["Y"] or 0) and cH or (self.ColumnSizes[j]["Y"] or 0));
 				end
 			else
-				cW = (self.ColumnSizes[column]["X"] or 0);
-				cH = (self.ColumnSizes[column]["Y"] or 0);
+				cW = (self.ColumnSizes[c]["X"] or 0);
+				cH = (self.ColumnSizes[c]["Y"] or 0);
 			end
 			-- Update column height/width.
 			cW = (cW == 0 and item:GetWidth() or cW <= 1 and (self:GetWidth() * cW));
 			cH = (cH == 0 and item:GetHeight() or cH <= 1 and (self:GetHeight() * cH));
 			-- Debug layout?
-			if(self.Debug == true) then self:DebugItem(item, cW, cH, cW-pL-pR, cH-pT-pB, pL, pR, pT, pB, mL, mR, mT, mB, offsetX, offsetY); end
+			if(self.Debug == true) then self:DebugItem(item, cW, cH, cW-pL-pR, cH-pT-pB, pL, pR, pT, pB, mL, mR, mT, mB, oX, oY); end
 			-- Update sizes, subtracting padding.
 			item:SetWidth(cW - pL - pR);
 			item:SetHeight(cH - pT - pB);
 			-- Set point.
 			item:ClearAllPoints();
-			item:SetPoint("TOPLEFT", self, "TOPLEFT", offsetX+pL+mL, (offsetY - pT - mT));
-			-- Update offsets once more for right/bottom padding and margins.
-			offsetX = offsetX + cW + mL + mR;
+			item:SetPoint("TOPLEFT", self, "TOPLEFT", oX+pL+mL, (oY - pT - mT));
+			-- Update offsets once more for margins only.
+			oX = oX + cW + mL + mR;
 			-- Use cH for vertical offset. The Y offset of each row is determined based on the largest object in the previous row.
 			cH = cH + mB + mT;
-			modY = (modY > cH and modY or cH);
+			mY = (mY > cH and mY or cH);
 			-- Column offset update.
-			columnOffset = item.LayoutOpts["Columns"];
+			cO = item.LayoutOpts["Columns"];
 		end
 	end
 	frame.DebugItem = function(self, item, fcW, fcH, cW, cH, pL, pR, pT, pB, mL, mR, mT, mB, oX, oY)
-	print(pL, pR, pT, pB, mL, mR, mT, mB, oX, oY);
 		local name = item:GetName();
 		for i=1, 9 do
 			-- Get texture/frame.
