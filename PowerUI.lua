845cf358a1d376edb815962adf68c58e2997292c
dyates92 <dyates92@localhost>
2011-03-22 22:50:25 +0000

    Added debug mode for layout frames, will draw coloured solid textures above the elements to display their size, padding and margins. Fixed multiple padding bugs with the layout code where padding would double up forcing elements to be offset twice as far.

--- PowerUI.lua
+++ PowerUI.lua
@@ -254,6 +254,7 @@ function PowaLayoutFrame_Init(frame)
 	frame.Columns = 1;
 	frame.ColumnSizes = {};
 	frame.Items = {};
+	frame.Debug = false;
 	-- Sets the columns for the grid. Defaults to 1, can specify a sizes table.
 	frame.SetColumns = function(self, columns, sizes)
 		self.Columns = columns;
@@ -288,22 +289,22 @@ function PowaLayoutFrame_Init(frame)
 		local itemCount, column, columnOffset, offsetY, offsetX, modY = #(self.Items), 0, 1, 0, 0, 0;
 		for i=1,itemCount do
 			-- Some locals for width, height and the item...
-			local item, colWidth, colHeight = self.Items[i], 0, 0;
+			local item, cW, cH = self.Items[i], 0, 0;
 			-- Store padding/margins in locals, as it's a long thing to write out and we need to calculate stuff from it a lot.
-			local paddingLeft, paddingRight, paddingTop, paddingBottom = item.LayoutOpts["Padding"][1], 
+			local pL, pR, pT, pB = item.LayoutOpts["Padding"][1], 
 				item.LayoutOpts["Padding"][2], item.LayoutOpts["Padding"][3], item.LayoutOpts["Padding"][4];
-			local marginLeft, marginRight, marginTop, marginBottom = item.LayoutOpts["Margin"][1], 
+			local mL, mR, mT, mB = item.LayoutOpts["Margin"][1], 
 				item.LayoutOpts["Margin"][2], item.LayoutOpts["Margin"][3], item.LayoutOpts["Margin"][4];
 			-- Calculate the padding/margins if they're fluid values.
 			-- The only difference in these fluid values is they are not fluid if they equal 1.
-			paddingLeft = (paddingLeft > 0 and paddingLeft < 1 and self:GetWidth() * paddingLeft or paddingLeft);
-			paddingRight = (paddingRight> 0 and paddingRight < 1 and self:GetWidth() * paddingRight or paddingRight);
-			paddingTop = (paddingTop > 0 and paddingTop < 1 and self:GetWidth() * paddingTop or paddingTop);
-			paddingBottom = (paddingBottom > 0 and paddingBottom < 1 and self:GetWidth() * paddingBottom or paddingBottom);
-			marginLeft = (marginLeft > 0 and marginLeft < 1 and self:GetWidth() * marginLeft or marginLeft);
-			marginRight = (marginRight > 0 and marginRight < 1 and self:GetWidth() * marginRight or marginRight);
-			marginTop = (marginTop > 0 and marginTop < 1 and self:GetWidth() * marginTop or marginTop);
-			marginBottom = (marginBottom > 0 and marginBottom < 1 and self:GetWidth() * marginBottom or marginBottom);
+			pL = (pL > 0 and pL < 1 and self:GetWidth() * pL or pL);
+			pR = (pR > 0 and pR < 1 and self:GetWidth() * pR or pR);
+			pT = (pT > 0 and pT < 1 and self:GetWidth() * pT or pT);
+			pB = (pB > 0 and pB < 1 and self:GetWidth() * pB or pB);
+			mL = (mL > 0 and mL < 1 and self:GetWidth() * mL or mL);
+			mR = (mR > 0 and mR < 1 and self:GetWidth() * mR or mR);
+			mT = (mT > 0 and mT < 1 and self:GetWidth() * mT or mT);
+			mB = (mB > 0 and mB < 1 and self:GetWidth() * mB or mB);
 			-- Update column offset.
 			if(self.Columns < (column+columnOffset)) then
 				column = 1;
@@ -316,36 +317,105 @@ function PowaLayoutFrame_Init(frame)
 			-- Calculate column height and width, obeying column span rules.
 			-- If the size is <= 1, then it's a fluid value based on container size. If no size is specified, default to item size.
 			if(column+(item.LayoutOpts["Columns"]-1) <= self.Columns) then
-				for i=column, column+(item.LayoutOpts["Columns"]-1) do
-					colWidth = colWidth + (self.ColumnSizes[i]["X"] or 0);
-					colHeight = (colHeight > (self.ColumnSizes[i]["Y"] or 0) and colHeight or (self.ColumnSizes[i]["Y"] or 0));
+				for j=column, column+(item.LayoutOpts["Columns"]-1) do
+					cW = cW + (self.ColumnSizes[j]["X"] or 0);
+					cH = (cH > (self.ColumnSizes[j]["Y"] or 0) and cH or (self.ColumnSizes[j]["Y"] or 0));
 				end
 			else
-				colWidth = (self.ColumnSizes[column]["X"] or 0);
-				colHeight = (self.ColumnSizes[column]["Y"] or 0);
-			end
-			if(item.LayoutOpts["Columns"] > 1) then
-				print(colWidth, colHeight, paddingLeft, paddingRight, paddingTop, paddingBottom, marginLeft, marginRight, marginTop, marginBottom);
+				cW = (self.ColumnSizes[column]["X"] or 0);
+				cH = (self.ColumnSizes[column]["Y"] or 0);
 			end
 			-- Update column height/width.
-			colWidth = (colWidth == 0 and item:GetWidth() or colWidth <= 1 and (self:GetWidth() * colWidth));
-			colHeight = (colHeight == 0 and item:GetHeight() or colHeight <= 1 and (self:GetHeight() * colHeight));
+			cW = (cW == 0 and item:GetWidth() or cW <= 1 and (self:GetWidth() * cW));
+			cH = (cH == 0 and item:GetHeight() or cH <= 1 and (self:GetHeight() * cH));
+			-- Debug layout?
+			if(self.Debug == true) then self:DebugItem(item, cW, cH, cW-pL-pR, cH-pT-pB, pL, pR, pT, pB, mL, mR, mT, mB, offsetX, offsetY); end
 			-- Update sizes, subtracting padding.
-			item:SetWidth(colWidth - paddingLeft - paddingRight);
-			item:SetHeight(colHeight - paddingTop - paddingBottom);
-			-- Update X offset to take into account the left padding and margins.
-			-- Do not modify the Y offset as this will push all further items down. Instead just modify the offset in the SetPoint call.
-			offsetX = offsetX + paddingLeft + marginLeft;
+			item:SetWidth(cW - pL - pR);
+			item:SetHeight(cH - pT - pB);
 			-- Set point.
 			item:ClearAllPoints();
-			item:SetPoint("TOPLEFT", self, "TOPLEFT", offsetX, (offsetY - paddingTop - marginTop));
+			item:SetPoint("TOPLEFT", self, "TOPLEFT", offsetX+pL+mL, (offsetY - pT - mT));
 			-- Update offsets once more for right/bottom padding and margins.
-			offsetX = offsetX + colWidth + paddingRight + marginRight;
-			-- Use colHeight for vertical offset. The Y offset of each row is determined based on the largest object in the previous row.
-			colHeight = colHeight + paddingBottom + marginBottom + paddingTop + marginTop;
-			modY = (modY > colHeight and modY or colHeight);
+			offsetX = offsetX + cW + mL + mR;
+			-- Use cH for vertical offset. The Y offset of each row is determined based on the largest object in the previous row.
+			cH = cH + mB + mT;
+			modY = (modY > cH and modY or cH);
 			-- Column offset update.
 			columnOffset = item.LayoutOpts["Columns"];
+		end
+	end
+	frame.DebugItem = function(self, item, fcW, fcH, cW, cH, pL, pR, pT, pB, mL, mR, mT, mB, oX, oY)
+	print(pL, pR, pT, pB, mL, mR, mT, mB, oX, oY);
+		local name = item:GetName();
+		for i=1, 9 do
+			-- Get texture/frame.
+			local frame = item["Debug" .. i] or CreateFrame("Frame", nil, item);
+			frame:EnableMouse(true);
+			frame:SetFrameStrata("HIGH");
+			frame.Texture = frame:CreateTexture();
+			frame.Texture:SetAllPoints(frame);
+			-- Are we doing size, padding or margins?
+			if(i == 1) then
+				-- Size.
+				frame.Texture:SetTexture(1, 0, 0, 1);
+				frame:SetAllPoints(item); -- Just do a setallpoints for width/height...				
+				PowaAuras.UI.Tooltip(frame, item:GetName(), "Width: " .. fcW .. "\nHeight: " .. fcH .. 
+					"\nDisplayed Width: " .. cW .. "\nDisplayed Height: " .. cH .. 
+					"\n\nPadding (L): " .. pL .. "\nPadding (R): " .. pR .. "\nPadding (T): " .. pT .. "\nPadding (B): " .. pB .. 
+					"\n\nMargin (L): " .. mL .. "\nMargin (R): " .. mR .. "\nMargin (T): " .. mT .. "\nMargin (B): " .. mB .. 
+					"\n\nOffset (X): " .. oX .. "\nOffset (Y): " .. oY .. "\nDisplayed Offset (X): " .. oX+pL+mL .. "\nDisplayed Offset (Y): " .. oY-pT-mT);
+			elseif(i >= 2 and i <= 5) then
+				-- Padding.
+				frame.Texture:SetTexture(0, 1, 0, 1);
+				if(i == 2) then
+					frame:SetPoint("RIGHT", item, "LEFT");
+					PowaAuras.UI.Tooltip(frame, item:GetName(), "Padding (L): " .. pL);
+					frame:SetWidth(math.abs(pL));
+					frame:SetHeight(math.abs(cH));
+				elseif(i == 3) then
+					frame:SetPoint("LEFT", item, "RIGHT");
+					PowaAuras.UI.Tooltip(frame, item:GetName(), "Padding (R): " .. pR);
+					frame:SetWidth(math.abs(pR));
+					frame:SetHeight(math.abs(cH));
+				elseif(i == 4) then
+					frame:SetPoint("BOTTOM", item, "TOP");
+					PowaAuras.UI.Tooltip(frame, item:GetName(), "Padding (T): " .. pT);
+					frame:SetHeight(math.abs(pT));
+					frame:SetWidth(math.abs(cW));
+				elseif(i == 5) then
+					frame:SetPoint("TOP", item, "BOTTOM");
+					PowaAuras.UI.Tooltip(frame, item:GetName(), "Padding (B): " .. pB);
+					frame:SetHeight(math.abs(pB));
+					frame:SetWidth(math.abs(cW));
+				end
+			elseif(i >= 6 and i <= 9) then
+				-- Margins.
+				frame.Texture:SetTexture(0, 0, 1, 1);
+				if(i == 6) then
+					frame:SetPoint("RIGHT", item, "LEFT", -pL, 0);
+					PowaAuras.UI.Tooltip(frame, item:GetName(), "Margin (L): " .. mL);
+					frame:SetWidth(math.abs(mL));
+					frame:SetHeight(math.abs(cH));
+				elseif(i == 7) then
+					frame:SetPoint("LEFT", item, "RIGHT", pR, 0);
+					PowaAuras.UI.Tooltip(frame, item:GetName(), "Margin (R): " .. mR);
+					frame:SetWidth(math.abs(mR));
+					frame:SetHeight(math.abs(cH));
+				elseif(i == 8) then
+					frame:SetPoint("BOTTOM", item, "TOP", 0, pT);
+					PowaAuras.UI.Tooltip(frame, item:GetName(), "Margin (T): " .. mT);
+					frame:SetHeight(math.abs(mT));
+					frame:SetWidth(math.abs(cW));
+				elseif(i == 9) then
+					frame:SetPoint("TOP", item, "BOTTOM", 0, -pB);
+					PowaAuras.UI.Tooltip(frame, item:GetName(), "Margin (B): " .. mB);
+					frame:SetHeight(math.abs(mB));
+					frame:SetWidth(math.abs(cW));
+				end
+			end
+			-- Store texture...
+			item["Debug" .. i] = frame;
 		end
 	end
 end
