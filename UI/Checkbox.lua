1a165fc041f68a1d0acb45e19b56b4a7effa721c
Smacker <Smacker@localhost>
2011-05-03 23:09:20 +0100

    Prevent Text auras from animating (temp until Blizz pull their finger out)

---
+++ UI/Checkbox.lua
@@ -0,0 +1,69 @@
+-- Create definition.
+PowaAuras.UI:Register("Checkbox", {
+	Hooks = {
+		"SetChecked",
+	},
+	EnhancedScripts = true,
+	Scripts = {
+		OnClick = true,
+	},
+	Init = function(self, property, invert, tooltipDesc)
+		-- Update text to the localized variant.
+		local localeKey = self:GetText();
+		self:SetText(PowaAuras.Text[localeKey]);
+		-- Store invert property.
+		self.Invert = invert or false;
+		-- Register Settings mixin.
+		PowaAuras.UI:Settings(self, property);
+		-- Add tooltip.
+		PowaAuras.UI:Tooltip(self, localeKey, tooltipDesc or localeKey .. "Desc");
+		-- Update colours...
+		self:UpdateColors();
+	end,
+	OnClick = function(self)
+		self:SaveSetting(self:GetChecked());
+	end,
+	OnSettingChanged = function(self, value)
+		--  Update based on invert state.
+		if(self.Invert) then
+			self:SetChecked(not value);
+		else
+			self:SetChecked(value);
+		end
+	end,
+	SaveSetting = function(self, value)
+		if(self.Invert) then
+			PowaAuras.Helpers:SaveSetting(self.SettingKey, not self:GetChecked());
+		else
+			PowaAuras.Helpers:SaveSetting(self.SettingKey, (self:GetChecked() and true or false));
+		end
+	end,
+	SetChecked = function(self, checked)
+		-- Update.
+		self:__SetChecked(checked);
+		self:UpdateColors();
+	end,
+	UpdateColors = function(self)
+		if(self:GetChecked()) then
+			if(self:IsMouseOver()) then
+				self.BorderLeft:SetVertexColor(1, 0.82, 0, 1);
+				self.BorderTop:SetGradientAlpha("HORIZONTAL", 1, 0.82, 0, 1, 1, 0.82, 0, 0);
+				self.BorderBottom:SetGradientAlpha("HORIZONTAL", 1, 0.82, 0, 1, 1, 0.82, 0, 0);
+			else
+				self.BorderLeft:SetVertexColor(1, 0.82, 0, 0.8);
+				self.BorderTop:SetGradientAlpha("HORIZONTAL", 1, 0.82, 0, 0.8, 1, 0.82, 0, 0);
+				self.BorderBottom:SetGradientAlpha("HORIZONTAL", 1, 0.82, 0, 0.8, 1, 0.82, 0, 0);
+			end
+		else
+			if(self:IsMouseOver()) then
+				self.BorderLeft:SetVertexColor(0.3, 0.3, 0.3, 1);
+				self.BorderTop:SetGradientAlpha("HORIZONTAL", 0.3, 0.3, 0.3, 1, 0.3, 0.3, 0.3, 0);
+				self.BorderBottom:SetGradientAlpha("HORIZONTAL", 0.3, 0.3, 0.3, 1, 0.3, 0.3, 0.3, 0);
+			else
+				self.BorderLeft:SetVertexColor(0.3, 0.3, 0.3, 0.8);
+				self.BorderTop:SetGradientAlpha("HORIZONTAL", 0.3, 0.3, 0.3, 0.8, 0.3, 0.3, 0.3, 0);
+				self.BorderBottom:SetGradientAlpha("HORIZONTAL", 0.3, 0.3, 0.3, 0.8, 0.3, 0.3, 0.3, 0);
+			end
+		end
+	end
+});
---
+++ UI/Mixins/Settings.lua
@@ -0,0 +1,35 @@
+-- Create definition.
+PowaAuras.UI:Register("Settings", {
+	Construct = function(class, _, frame, setting)
+		-- Place in default functions.
+		frame.GetSetting = frame.GetSetting or class.GetSetting;
+		frame.SaveSetting = frame.SaveSetting or class.SaveSetting;
+		frame.UpdateSetting = frame.UpdateSetting or class.UpdateSetting;
+		frame.OnSettingChanged = frame.OnSettingChanged or class.OnSettingChanged;
+		-- Register setting.
+		frame:UpdateSetting(setting);
+		-- We're done.
+		return;
+	end,
+	GetSetting = function(self)
+		return PowaAuras.Helpers:GetSetting(self.SettingKey);
+	end,
+	SaveSetting = function(self, value)
+		return PowaAuras.Helpers:SaveSetting(self.SettingKey, value);
+	end,
+	UpdateSetting = function(self, setting)
+		-- Store setting.
+		self.SettingKey = setting;
+		-- Only register the callback once.
+		if(not self.HasRegisteredSettingsCallback) then
+			-- Go go go.
+			PowaAuras.Helpers:RegisterSettingCallback(function(key, value)
+				if(self.SettingKey ~= key) then return; end
+				self:OnSettingChanged(value);
+			end);
+			self.HasRegisteredSettingsCallback = true;
+		end
+		-- Trigger update.
+		self:OnSettingChanged(self:GetSetting());
+	end,
+});
