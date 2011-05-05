f2d69c5c209612c21abf657cdba808f73c902c7c
dyates92 <dyates92@localhost>
2011-05-05 19:04:39 +0100

    4.18 ==> 4.19 Patch 4.2 has another bloody combat log API change. Backwards compat mode is GO.

--- PowerAurasEvents.lua
+++ PowerAurasEvents.lua
@@ -666,8 +666,22 @@ end
 function PowaAuras:COMBAT_LOG_EVENT_UNFILTERED(...)
 	--self:ShowText("COMBAT_LOG_EVENT_UNFILTERED");
 	if (self.ModTest) then return end
-	-- 4.1 backwards compat (will remove when 4.1 is live, prevents needing to time a release to fix this).
-	local timestamp,event,casterHidden,sourceGUID,sourceName,sourceFlags,destGUID,destName,destFlags, spellId, spellName, _, spellType = ...;		
+	-- 4.2 changes.
+	local timestamp, event, casterHidden, 
+		sourceGUID, sourceName, sourceFlags, sourceFlags2, 
+		destGUID, destName, destFlags, destFlags2, 
+		spellId, spellName, _, spellType;
+	if(self.WoWBuild >= 14002) then
+		timestamp, event, casterHidden, 
+		sourceGUID, sourceName, sourceFlags, sourceFlags2,
+		destGUID, destName, destFlags, destFlags2, 
+		spellId, spellName, _, spellType = ...;
+	else
+		timestamp, event, casterHidden, 
+		sourceGUID, sourceName, sourceFlags,
+		destGUID, destName, destFlags, 
+		spellId, spellName, _, spellType = ...;
+	end	
 	if (not spellName) then return end
 	--self:ShowText("CLEU: ", event, " by me=", sourceGUID==UnitGUID("player"), " on me=", destGUID==UnitGUID("player"), " ", spellName);
 	--self:ShowText("Player=", UnitGUID("player"), " sourceGUID=", sourceGUID, " destGUID=", destGUID);
