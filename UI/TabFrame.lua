d0ea83e82dbaed33135a1836aee5a886ea68d402
dyates92 <dyates92@localhost>
2011-06-21 03:42:46 +0100

    PowerAurasAuraClasses.lua:         Added Timer/Stacks dispose code to Dispose() function on cPowaAura. Code has been commented out as it needs testing.         Added CopyDecorators function to cPowaAura, it copies the Timer, Stacks and Triggers on the current aura to another aura with the specified ID. Trigger copying not yet implemented.         Added UpdateAuraID function to cPowaAura, which updates the aura ID assigned to the current auras Timer, Stacks and Triggers. PowerAurasBase.lua:         Added three variables/constants to global table - MaxAuras (maximum amount of auras), MaxAurasPerPage (maximum amount of auras on a page) and MaxPages (maximum number of pages). PowerHelpers.lua:         Added import function for creating auras from an import string. Currently not hooked up to anything.         Added functions for reindexing auras, and by extension copying auras from one location to another.         Heavily documented existing functions. UI/AuraBrowser.lua:         Added missing getter function. Manual Pics:         Removed all manual pics, they're going to be stupidly out of date by the time this hits. We should consider using the Help system to link to the Wiki for this. Documentation:         Added bash script for generating lua documentation via LuaDoc. Documentation is available in the docs folder, you can browse it by accessing docs/index.html. Currently only the PowerHelpers.lua file has been documented.

---
+++ UI/TabFrame.lua
@@ -0,0 +1,86 @@
+-- Generic tab frame mixin.
+PowaAuras.UI:Register("TabFrame", {
+	Init = function(self, class)
+		-- Store the name of the tab class we're using.
+		self.TabClass = class;
+		self.SelectedTab = 1;
+		-- And our tabs in here.
+		self.Tabs = {};
+		-- Scripts mixin.
+		PowaAuras.UI:Scripts(self);
+	end,
+	AddTab = function(self, frame, ...)
+		-- Create the tab.
+		tinsert(self.Tabs, PowaAuras.UI[self.TabClass](PowaAuras.UI, self, frame, ...));
+		-- Update.
+		self:UpdateTabs();
+	end,
+	ClearTabs = function(self)
+		-- Remove all tabs.
+		while(self.Tabs[1]) do
+			self:RemoveTab(1);
+		end
+	end,
+	GetSelectedTab = function(self)
+		return self.SelectedTab;
+	end,
+	HideTab = function(self, index)
+		-- Make sure it exists.
+		if(not self.Tabs[index]) then return; end
+		-- Well this is pretty simple. Note that you can still select hidden tabs programmatically.
+		self.Tabs[index]:Hide();
+	end,
+	RemoveTab = function(self, index)
+		-- Make sure it exists.
+		if(not self.Tabs[index]) then return; end
+		-- Remove the tab.
+		self.Tabs[index]:Recycle();
+		tremove(self.Tabs, index);
+		-- If this tab was selected, select index #1.
+		if(self.SelectedTab and self.SelectedTab == index) then
+			self:SetSelectedTab(1);
+		end
+		-- Update tabs.
+		self:UpdateTabs();
+	end,
+	SetSelectedTab = function(self, index)
+		-- Make sure the index exists, even if its associated tab doesn't.
+		if(not index or not self.Tabs[index]) then index = 1; end
+		-- Set value.
+		self.SelectedTab = index;
+		-- Callback.
+		self:CallScript("OnSelectedTabChanged", index);
+		-- Update tabs.
+		self:UpdateTabs();
+	end,
+	ShowTab = function(self, index)
+		-- Make sure it exists.
+		if(not self.Tabs[index]) then return; end
+		-- Well this is pretty simple.
+		self.Tabs[index]:Show();
+	end,
+	UpdateTabs = function(self, index)
+		-- Locals.
+		local offset = 0;
+		-- Position tabs.
+		for index, tab in pairs(self.Tabs) do
+			-- Update the tab index first of all.
+			tab:SetTabIndex(index);
+			-- Checked?
+			tab:SetChecked((self.SelectedTab and self.SelectedTab == index));
+			-- Position it if needed.
+			if(tab:IsShown()) then
+				tab:ClearAllPoints();
+				tab:SetPoint(
+					tab.Points[1],
+					self,
+					tab.Points[2], 
+					(tab.Points[3]+(tab.Orientation == "HORIZONTAL" and offset or 0)),
+					(tab.Points[4]+(tab.Orientation == "VERTICAL" and offset or 0))
+				);
+				-- Bump the offset.
+				offset = offset+tab.Offset;
+			end
+		end
+	end,
+});
